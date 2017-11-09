2c8a63ec237918d04b25297bab5ea50972c8326c
PATRICK .Py <pwalukagga@gmail.com>
2017-11-09 16:24:07 +0300

    test for search of category and category pagination

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -194,3 +194,121 @@ class TestCategoriesBlueprint(BaseTestCase):
                         str(response.data))
             self.assertIn('How to make breakfast', 
                         str(response.data))
+    
+    def test_user_retrieves_recipe_categories_with_limit(self):
+        """
+        Test for user retrieves recipe categories with limit pagination
+        """
+        with self.client:
+            response = self.register_user(
+                "Patrick", "Walukagga", 
+                "pwalukagga@gmail.com", "telnetcmd123"
+            )
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('Successfully registered', str(response.data))
+            self.assertIn('success', str(response.data))
+            # registered user login
+            registered_user = json.dumps({
+                "email": "pwalukagga@gmail.com",
+                "password": "telnetcmd123" 
+            })
+            rep_login = self.client.post(
+                'auth/login', data=registered_user, 
+                content_type='application/json'
+            )
+            self.assertEqual(rep_login.status_code, 200)
+            self.assertIn('Successfully logged in', 
+                            str(rep_login.data))
+            self.assertIn('success', str(rep_login.data))
+            # valid token
+            headers=dict(
+                Authorization='Bearer ' + json.loads(
+                    rep_login.data.decode()
+                )['auth_token']
+            )
+            category = RecipeCategory(
+                name="Morningfast",
+                description="How to make morningfast",
+                user_id=1
+            )
+            category.save()
+            category_data = json.dumps({"name": "Breakfast", 
+                                     "description": 
+                                     "How to make breakfast"})
+            response = self.client.post('/recipe_category', 
+                                        headers=headers,
+                                        data=category_data)
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('New recipe category created!', 
+                        str(response.data))
+            category_data = json.dumps({"name": "Lunchfast", 
+                                     "description": 
+                                     "How to make lunchfast"})
+            response = self.client.post('/recipe_category', 
+                                        headers=headers,
+                                        data=category_data)
+            response = self.client.get('/recipe_category?limit=2', 
+                                        headers=headers,
+                                        data=category_data)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('How to make morningfast', 
+                        str(response.data))
+            self.assertIn('How to make breakfast', 
+                        str(response.data))
+            self.assertNotIn('How to make lunchfast', 
+                        str(response.data))
+    
+    def test_user_retrieves_recipe_categories_with_search(self):
+        """
+        Test for user retrieves recipe categories with search
+        """
+        with self.client:
+            response = self.register_user(
+                "Patrick", "Walukagga", 
+                "pwalukagga@gmail.com", "telnetcmd123"
+            )
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('Successfully registered', str(response.data))
+            self.assertIn('success', str(response.data))
+            # registered user login
+            registered_user = json.dumps({
+                "email": "pwalukagga@gmail.com",
+                "password": "telnetcmd123" 
+            })
+            rep_login = self.client.post(
+                'auth/login', data=registered_user, 
+                content_type='application/json'
+            )
+            self.assertEqual(rep_login.status_code, 200)
+            self.assertIn('Successfully logged in', 
+                            str(rep_login.data))
+            self.assertIn('success', str(rep_login.data))
+            # valid token
+            headers=dict(
+                Authorization='Bearer ' + json.loads(
+                    rep_login.data.decode()
+                )['auth_token']
+            )
+            category_data = json.dumps({"name": "Breakfast", 
+                                     "description": 
+                                     "How to make breakfast"})
+            response = self.client.post('/recipe_category', 
+                                        headers=headers,
+                                        data=category_data)
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('New recipe category created!', 
+                        str(response.data))
+            category_data = json.dumps({"name": "Lunchfast", 
+                                     "description": 
+                                     "How to make lunchfast"})
+            response = self.client.post('/recipe_category', 
+                                        headers=headers,
+                                        data=category_data)
+            response = self.client.get('/recipe_category?q=Lunchfast', 
+                                        headers=headers,
+                                        data=category_data)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('How to make lunchfast', 
+                        str(response.data))
+            self.assertNotIn('How to make breakfast', 
+                        str(response.data))
