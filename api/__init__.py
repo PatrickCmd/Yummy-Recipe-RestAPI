12740616f465c8bce5c337d1c8fcc9a885ea42b2
PatrickCmd <pwalukagga@gmail.com>
2017-11-17 16:55:55 +0300

    flasgger and swagger configuration setup

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flasgger import Swagger
 
 from api.config import app_config
 
@@ -15,9 +16,64 @@ CORS(app)
 config_name = os.environ['APP_SETTINGS']
 app.config.from_object(app_config[config_name])
 app.config.from_pyfile('config.py')
+# swagger configuration
+app.config['SWAGGER'] = {
+    'title': 'YummyRecipes',
+        'description': "This project is part of the [Andela Fellowship](https://andela.com/)\
+    Bootcamp.\n\nYummy recipes app is an application that allows users to create, save and share\
+    meeting the needs of keeping track of awesome food recipes. \nThis is a REST API built in python using\
+    Flask.\
+    \n\nThe host is 'https://https://yummy-recipe-api.herokuapp.com/'\
+    \n\nThe Github repo at 'https://https://github.com/PatrickCmd/Yummy-Recipe-RestAPI",
+        'basePath': '/',
+        'version': '1.0.0',
+        'contact': {
+                    'responsibleOrganization': 'Andela',
+                    'responsibleDeveloper': 'WALUKAGGA PATRICK',
+                    'email': 'patrick.walukagga@andela.com',
+                    'url': 'https://andela.com/',
+                    },
+        'schemes': [
+                        'http',
+                        'https'
+                    ],
+        'license': {
+            'name': 'MIT',
+            'url': 'https://github.com/PatrickCmd/Yummy-Recipe-RestAPI/blob/master/LICENSE'
+        },
+        'tags': [
+            {
+                'name': 'Recipe category',
+                'description': 'A category of recipes of similar food'
+            },
+            {
+                'name': 'Recipe',
+                'description': 'A recipe of a kind of food'
+            },
+            {
+                'name': 'Recipe step',
+                'description': 'A step in a given food recipe'
+            },            
+            {
+                'name': 'User_Authentication',
+                'description': 'Register, login/authenticate user, reset password and logout user'
+            },
+        ],
+        'specs_route': '/apidocs/'    
+}
 
 bcrypt = Bcrypt(app)
 db.init_app(app)
+swagger = Swagger(app)
+
+# index route
+@app.route('/')
+@app.route('/index')
+def index():
+    """
+    Redirect to the root of the swagger docs
+    """
+    return redirect('/apidocs/')
 
 
 from api.auth.views import auth_blueprint
