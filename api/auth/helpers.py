ab3d7a7121a175941708c8ef057c13c5cdbd1dab
PatrickCmd <pwalukagga@gmail.com>
2017-11-21 16:07:21 +0300

    implement validate email address

--- api/auth/helpers.py
+++ api/auth/helpers.py
@@ -1,6 +1,15 @@
 # api/auth/helpers.py
+import re
 
 # method to check for special characters and validate a name
 def is_valid(name_string):
     special_character = "~!@#$%^&*()_={}|\[]<>?/,;:"
     return any(char in special_character for char in name_string)
+
+# method to check whether email is valid
+def is_valid_email(email):
+    match=re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]*\.*[com|org|edu]{3}$)", email)
+    if match:
+        return email
+    else:
+        return 'Invalid email'
