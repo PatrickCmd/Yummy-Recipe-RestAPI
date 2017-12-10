6ce8f87a17d7fa8a8b4a029aa33559009aa79c60
PatrickCmd <pwalukagga@gmail.com>
2017-12-10 10:11:48 +0300

    add helpers for data validation

--- api/auth/helpers.py
+++ api/auth/helpers.py
@@ -3,6 +3,8 @@ import re
 
 from flask import abort, jsonify
 
+from api import app
+
 # method to check for special characters and validate a name
 def is_valid(name_string):
     special_character = "~!@#$%^&*()_={}|\[]<>?/,;:"
@@ -27,8 +29,39 @@ def key_missing_in_body(data):
         if key not in data:
             abort(400)
 
+def login_key_missing_in_body(data):
+    # check if key is present in data
+    keys = ('email', 'password')
+    for key in keys:
+        if key not in data:
+            abort(400)
+
+def category_key_missing_in_body(data):
+    # check if key is present in data
+    keys = ('name', 'description')
+    for key in keys:
+        if key not in data:
+            abort(400)
+
+def recipe_key_missing_in_body(data):
+    # check if key is present in data
+    keys = ('name', 'ingredients', 'description')
+    for key in keys:
+        if key not in data:
+            abort(400)
+
 def key_is_not_string(data):
     "check if key is string"
     for key in data:
         if not isinstance(data[key], str):
             return True
+
+# check if endpoint url_rule is correct
+'''def not_correct_urlrule(url):
+    rules = app.url_map.iter_rules()
+    rules = list(rules)
+    rule_list = []
+    for rule in rules:
+        rule_list.append(str(rule))
+    if url not in rule_list:
+        return url'''
